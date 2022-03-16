ff05ef29bcb975fffe11dc39dd96061cdcfdf27e
arlly0000 <zm73aym@gmail.com>
2022-03-16 21:57:33 +0900

    edit .circleci/config.yml to run Serverspec

--- server_spec/spec/practice/sample_spec.rb
+++ server_spec/spec/practice/sample_spec.rb
@@ -11,18 +11,18 @@ describe package("nodejs") do
   it { should be_installed.with_version("16") }
 end
 
-# describe package("nginx"), :if => os[:family] == "amazon" do
-#   it { should be_installed }
-# end
+describe package("nginx"), :if => os[:family] == "amazon" do
+  it { should be_installed }
+end
 
 describe file("/etc/nginx/conf.d/rails.conf") do
   it { should exist }
 end
 
-# describe service("nginx"), :if => os[:family] == "amazon" do
-#   it { should be_enabled }
-#   it { should be_running }
-# end
+describe service("nginx"), :if => os[:family] == "amazon" do
+  it { should be_enabled }
+  it { should be_running }
+end
 
 describe file("/srv/rails-app") do
   it { should be_directory }
