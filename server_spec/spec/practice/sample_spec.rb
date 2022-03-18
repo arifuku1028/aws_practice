77040e430d34055b96e0228954024b7e38e2c408
arlly0000 <zm73aym@gmail.com>
2022-03-18 10:23:57 +0900

    setting Ansible

--- server_spec/spec/practice/sample_spec.rb
+++ server_spec/spec/practice/sample_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-%w{git gcc gcc-c++ patch rpm-build mariadb mariadb-devel gdbm-devel zlib-devel openssl-devel ruby-devel rubygems-devel ImageMagick-devel readline-devel libcurl-devel libffi-devel libicu-devel libxml2-devel libxslt-devel libyaml-devel}.each do |pkg|
+%w{git gcc gcc-c++ patch rpm-build mariadb mariadb-devel gdbm-devel zlib-devel openssl-devel ImageMagick-devel readline-devel libcurl-devel libffi-devel libicu-devel libxml2-devel libxslt-devel libyaml-devel}.each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
