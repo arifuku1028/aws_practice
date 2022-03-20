a6ede9c3cca87ce1513a5ceb8e52fc7b0fb76a9c
arlly0000 <zm73aym@gmail.com>
2022-03-20 17:09:09 +0900

    modify .circleci/config.yml

--- server_spec/spec/base/connection_spec.rb
+++ server_spec/spec/base/connection_spec.rb
@@ -8,6 +8,6 @@ describe port(22) do
   it { should be_listening }
 end
 
-describe command('curl practice-elb-884924591.ap-northeast-1.elb.amazonaws.com -o /dev/null -w "%{http_code}\n" -s') do
+describe command('curl practice-elb-789070725.ap-northeast-1.elb.amazonaws.com -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
